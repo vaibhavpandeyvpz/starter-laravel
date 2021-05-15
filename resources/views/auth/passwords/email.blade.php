bbcf623eee9c991093dcb4837cfc0e1bb270c55a
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-05-15 17:02:06 +0530

    Show alert on password reset email.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,6 +5,11 @@
 @endsection
 
 @section('content')
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
     <div class="card shadow-sm">
         <div class="card-body">
             <h5 class="card-title text-primary">{{ __('Forgot Password') }}</h5>
